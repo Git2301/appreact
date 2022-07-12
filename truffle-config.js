a814e69acc5ade938ebec6d3e77abd548b5e4c57
Nivetha <nivetha.s1701@gmail.com>
2022-07-13 02:35:31 +0530

    update
    update

---
+++ truffle-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // for more about customizing your Truffle configuration!
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*", // Match any network id
+      //from:'0xE0cfc67439eB2990A3A906dF6bC78E0849B71320'
+      //from:'0xe908fb5DE5be3A33a6Eb27a9dcc047B521454fAF'
+    }
+  }
+};
